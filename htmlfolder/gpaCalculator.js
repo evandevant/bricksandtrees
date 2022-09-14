49b53210cdc03636718ee2fb726f8217ba3f4aec
Evan Brown <104607653+evanhaxu@users.noreply.github.com>
2022-09-14 15:26:36 -0700

    commit

--- htmlfolder/gpaCalculator.js
+++ htmlfolder/gpaCalculator.js
@@ -3,15 +3,14 @@
 
     function addRow() {
 
-        var table = document.getElementById("gpaCalc");
-       
-        var row = table.insertRow(2);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
+        const table = document.getElementById("gpaCalc");
+        const row = document.getElementById("clone");
 
-       
+        const clone = row.cloneNode(true);
+        clone.id ="clone2";
+        table.appendChild(clone);
+        
+
     }
     
       
